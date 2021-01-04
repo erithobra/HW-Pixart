675c029a3bfd9056dd9d8fa64772f026a5bd871b
ericb2020 <ebrauer83@gmail.com>
2021-01-04 16:21:07 -0600

    1. Get color setting working

--- pixart/pixart.js
+++ pixart/pixart.js
@@ -1,1 +1,11 @@
+console.log("js file is linked");
 
+const setColor = document.querySelector("form");
+
+setColor.addEventListener("submit", function(evt){
+    evt.preventDefault();
+    const colorField = document.querySelector("#color-field");
+    console.log(colorField.value);
+    const colorBox = document.querySelector(".brush");
+    colorBox.style.background = colorField.value;
+})
